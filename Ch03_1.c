daa6e5fad7877efc53dc3a96a87163c8bb557ca1
woosoung4@naver.com <woosoung4@naver.com>
2023-06-29 17:51:02 +0900

    jijeonil

--- Ch03_1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(){
-int N;
-printf("%d:%d",N,10);
-return 0;
-}
--- Ch03_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-int N;
-
-scanf("%d", &N); //입력
-printf("%d, %d", N, N); //출력
-return 0;
-}
